87f6a94db61dd881b7adb72b2c465656cdd0afb3
Kate Schumacher <kate.schumacher@gmail.com>
2025-01-21 17:21:49 -0600

    Print weather data from API call to console

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("hello")
+async function getWeather(city, unitGroup) {
+    const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city ? city : "chicago"}?unitGroup=${unitGroup ? unitGroup : "us"}&key=8QAPDZ3XLKRU4CK8QAH8JRN5J`, {
+        mode: "cors"
+    });
+    if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+    let data = await response.json();
+    console.log(data);
+    console.log("Current weather: " + data.currentConditions.conditions);
+}
+
+getWeather("manila", "us");
