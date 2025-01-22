c23e3477a499408984744802574a89fbafda422f
Kate Schumacher <kate.schumacher@gmail.com>
2025-01-22 11:43:07 -0600

    Add gif search results

--- script.js
+++ script.js
@@ -2,15 +2,37 @@ async function getWeather(city, unitGroup) {
     const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city ? city : "chicago"}?unitGroup=${unitGroup ? unitGroup : "us"}&key=8QAPDZ3XLKRU4CK8QAH8JRN5J`, {
         mode: "cors"
     });
+
     if (!response.ok) {
         if (response.status === 400) {
             throw new Error("Bad Request: Please enter a valid city name.");
         }
         throw new Error(`HTTP error! Status: ${response.status}`);
     }
+
     let data = await response.json();
+
     console.log(data);
-    console.log("Current weather: " + data.currentConditions.conditions);
+    let currentWeather = data.currentConditions.conditions;
+    console.log("Current weather: " + currentWeather);
+    getGif(currentWeather);
+}
+
+async function getGif(term) {
+    const gifSlot = document.querySelector("img");
+
+    const response = await fetch(`https://api.giphy.com/v1/gifs/translate?api_key=wBRqeRe5x8K9uskZi2lOIXAMt5clY4Sf&s=${term ? term + " weather" : "weather"}`, {
+        mode: "cors"
+    });
+    console.log(term + " weather");
+
+    if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+    
+    const data = await response.json();
+
+    gifSlot.src = data.data.images.original.url;
 }
 
 let form = document.getElementById("form");
